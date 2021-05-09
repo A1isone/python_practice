25b8fc5da78bc3218763bfdb3ebf7ddc193e08dc
Joseph Salvador <1Salvador.Joseph@gmail.com>
2021-05-09 12:05:43 -0400

    Add files via upload
    init commit

---
+++ python_work/name_cases.py
@@ -0,0 +1,6 @@
+dude = "bro" 
+name = "man"
+dudesname = f"{dude}+{name}"
+print(f"Hello, {dudesname.title} What do it do?")
+print(dudesname.lower)
+print(dudesname.upper)
---
+++ python_work/strings.py
@@ -0,0 +1,9 @@
+#print("Languages:\n\tPython\n\tC\n\tJavaScript")
+
+favorite_language = ' python '
+favorite_language.rstrip()
+print(favorite_language.rstrip)
+favorite_language.lstrip()
+print(favorite_language.lstrip)
+favorite_language.strip()
+print(favorite_language.strip)
